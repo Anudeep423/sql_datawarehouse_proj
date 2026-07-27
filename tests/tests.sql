37352ba7285d453612f2bb3392377aead5863608
Anudeep Marlapalli <63209848+Anudeep423@users.noreply.github.com>
2026-07-27 12:36:41 +0530

    Update tests.sql

--- tests/tests.sql
+++ tests/tests.sql
@@ -39,3 +39,60 @@ from silver.crm_cust_info;
 
 SELECT DISTINCT cst_marital_status
 from silver.crm_cust_info;
+
+
+
+
+
+-- Test for silver.crm.prd_info 
+
+
+
+
+
+
+
+
+
+
+
+--- checking if prd_id has any null or duplicates
+SELECT prd_id,
+count(*) as prd_id_count from bronze.crm_prd_info
+group by prd_id
+having count(*) > 1 or prd_id is null;
+
+
+-- Check if prd_nm has any unwanted spaces
+
+SELECT prd_nm
+from bronze.crm_prd_info
+where prd_nm != Trim(prd_nm)
+
+-- Check of prd_cost has any null 
+
+SELECT prd_cost
+from bronze.crm_prd_info
+where prd_cost IS NULL;
+
+
+
+-- Lets check if the start date is less than end date
+
+
+SELECT * from bronze.crm_prd_info
+where prd_start_dt < prd_end_dt;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
