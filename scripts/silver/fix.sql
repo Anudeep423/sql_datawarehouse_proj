32c56c7e56a225626148d1f395e2ba681c95f8e9
Anudeep Marlapalli <63209848+Anudeep423@users.noreply.github.com>
2026-07-28 12:15:35 +0530

    Update fix.sql

--- scripts/silver/fix.sql
+++ scripts/silver/fix.sql
@@ -144,3 +144,57 @@ from bronze.crm_sales_details;
 
 
 
+
+
+
+
+-- insert the data into erp_cust_az12
+
+
+
+
+
+
+
+INSERT INTO silver.erp_cust_az12(
+cid,
+bdate,
+gen
+)
+SELECT
+CASE
+ WHEN cid like 'NAS%' THEN SUBSTRING(cid,4,len(cid))
+ else cid
+ end as cid,
+Case
+  when bdate > getdate() or bdate < '1926-01-01' then null
+  else bdate
+end as bdate,
+case
+ when upper(trim(gen)) in ('F','FEMALE') then 'Female'
+ when upper(trim(gen)) in ('M','MALE') then 'Male'
+ else 'n/a'
+end as gen
+from bronze.erp_cust_az12
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
